036b15d1db9ccd2c81fb2fce05b16a93809ae0a1
idosumit <psumitcoding@gmail.com>
2024-09-19 16:08:47 +0900

    Refactor CSS styles and add Meme component
    - Refactor CSS styles in App.css to improve readability and organization
    - Add Meme component to render a styled form with two inputs and a button
    - Update App.tsx to include the Meme component below the Header component
    - Create Meme.tsx file with the implementation of the Meme component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
+import Meme from "./components/Meme";
+
+/**
+ * Challenge:
+ * - Create a Meme component.
+ * - Inside the Meme component, render a styled form
+ *   with our 2 inputs and the button.
+ */
 
 function App() {
-  return <div> {<Header />}</div>;
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
 }
 
 export default App;
