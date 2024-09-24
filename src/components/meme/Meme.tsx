6d683b6dcb7f23576985d0ce00ce348b09476063
idosumit <psumitcoding@gmail.com>
2024-09-24 15:40:29 +0900

    creating and updating states

--- src/components/meme/Meme.tsx
+++ src/components/meme/Meme.tsx
@@ -1,51 +1,36 @@
 import memesData from "./memesData";
+import { useState } from "react";
 
 interface Meme {
   url: string;
   // add other properties if needed
 }
 
-export default function Meme() {
-  /**
-   * Challenge: Get a random image from the `memesData` array
-   * when the "new meme image" button is clicked.
-   *
-   * Log the URL of the image to the console. (Don't worry
-   * about displaying the image yet)
-   */
-
-  const getMemeImage = () => {
-    const randomIndex = Math.floor(Math.random() * memesData.data.memes.length);
-    const meme: Meme = memesData.data.memes[randomIndex];
-    console.log(meme.url);
+interface MemeData {
+  data: {
+    memes: Meme[];
   };
+}
+
+export default function Meme() {
+  const [memeImage, setMemeImage] = useState<string>("");
+
+  function getMemeImage() {
+    const memesArray: Meme[] = (memesData as MemeData).data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
 
   return (
     <main>
       <div className="form">
-        <div>
-          <label htmlFor="top-text">Top Text</label>
-          <input
-            id="top-text"
-            type="text"
-            placeholder="Shut up"
-            className="form--input"
-          />
-        </div>
-        <div>
-          <label>
-            Bottom Text
-            <input
-              type="text"
-              placeholder="and take my money"
-              className="form--input"
-            />
-          </label>
-        </div>
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼
         </button>
       </div>
+      {memeImage && <img src={memeImage} alt="Meme" className="meme--image" />}
     </main>
   );
 }
